2190b3beb5c6d427fae935cccf4e5998fd604627
mohamed nabil <58723734+MohamedNabil-14@users.noreply.github.com>
2023-02-19 15:31:24 +0200

    Portfolio-react

---
+++ src/Components/Projects.js
@@ -0,0 +1,101 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/port1.png";
+import projImg2 from "../assets/img/port2.png";
+import projImg3 from "../assets/img/port3.png";
+import projImg4 from "../assets/img/port4.png";
+import projImg5 from "../assets/img/port5.png";
+import projImg6 from "../assets/img/port6.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () =>{
+
+    const projects = [
+        {
+          title: "Project 1",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Project 2",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Project 3",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+        {
+          title: "Project 4",
+          description: "Design & Development",
+          imgUrl: projImg4,
+        },
+        {
+          title: "Project 5",
+          description: "Design & Development",
+          imgUrl: projImg5,
+        },
+        {
+          title: "Project 6",
+          description: "Design & Development",
+          imgUrl: projImg6,
+        },
+      ];
+   
+    return(
+        <section className="project" id="projects">
+        <Container>
+          <Row>
+            <Col size={12}>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                  <h2>Projects</h2>
+                  <p>Here is some of my work that I've done in various programming languages.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            projects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                  />
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                       <Tab.Pane eventKey="second">
+                        <p>No other fields</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                      <p>No other fields</p>
+                      </Tab.Pane> 
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>}
+              </TrackVisibility>
+            </Col>
+          </Row>
+        </Container>
+        <img className="background-image-right" src={colorSharp2} alt="colorSharp2"></img>
+      </section>
+    )
+}
